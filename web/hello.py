0f724ff474bcf6af0b4451ed9263bc63c8a7c89a
higlader <wton1984@mail.ru>
2016-03-11 11:01:14 +0300

    все переделал

---
+++ web/hello.py
@@ -0,0 +1,6 @@
+def app(env, start_response):
+  status = '200 OK'
+  headers = [('Content-Type', 'text/plain')]
+  body = env['QUERY_STRING'].replace('&', '\n')
+  start_response(status, headers)
+  return [body]
--- web/init.sh
+++ web/init.sh
@@ -1,6 +1,11 @@
-if [ -e /etc/nginx/sites-enabled/default ] ; then
-    sudo rm /etc/nginx/sites-enabled/default
-fi
+#if [ -e /etc/nginx/sites-enabled/default ] ; then
+#    sudo rm /etc/nginx/sites-enabled/default
+#fi
 
+#sudo ln -sf /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/test.conf
+#sudo /etc/init.d/nginx restart
 sudo ln -sf /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/test.conf
 sudo /etc/init.d/nginx restart
+sudo ln -sf /home/box/web/etc/hello.py /etc/gunicorn.d/hello.py
+sudo gunicorn -c /etc/gunicorn.d/hello.py hello:application
+sudo /etc/init.d/gunicorn restart
