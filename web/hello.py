58c05744d7631a8bb3594302031338d6e027471b
higlader <wton1984@mail.ru>
2016-03-21 15:51:21 +0300

    все задания

--- web/hello.py
+++ web/hello.py
@@ -1,6 +1,17 @@
-def app(env, start_response):
-  status = '200 OK'
-  headers = [('Content-Type', 'text/plain')]
-  body = env['QUERY_STRING'].replace('&', '\n')
-  start_response(status, headers)
-  return [body]
+#def app(env, start_response):
+  #status = '200 OK'
+  #headers = [('Content-Type', 'text/plain')]
+  #body = env['QUERY_STRING'].replace('&', '\n')
+  #start_response(status, headers)
+ # return [body]
+def app(environ, start_response):
+    """Simplest possible application object"""
+    data = '\n'.join(environ['QUERY_STRING'].split('&'))
+    #data = 'Hello, World!\n'
+    status = '200 OK'
+    response_headers = [
+        ('Content-type','text/plain'),
+        ('Content-Length', str(len(data)))
+    ]
+    start_response(status, response_headers)
+    return iter([data])
